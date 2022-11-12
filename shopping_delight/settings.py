a4e270e3b8ef52ba45d2e1f24c90b5b1929198bc
Chrisloufa <chris.mcgarry3@gmail.com>
2022-11-12 08:41:33 +0000

    removed secret key and set debug

--- shopping_delight/settings.py
+++ shopping_delight/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_z2ld*!=ltq%^*0h^hxa6l$6tw&r5-jjsi5mm4=dtn$ms7=tv7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shopping-delight.herokuapp.com', 'localhost']
 
