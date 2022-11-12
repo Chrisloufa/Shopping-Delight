0bdd755209ba9e7b73a1401b61e56040b57fbf00
Chrisloufa <chris.mcgarry3@gmail.com>
2022-11-12 08:22:17 +0000

    deployment part 2

--- shopping_delight/settings.py
+++ shopping_delight/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_z2ld*!=ltq%^*0h^hxa6l$6tw&r5-jjsi5mm4=dtn$ms7=tv7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shopping-delight.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -106,12 +106,17 @@ WSGI_APPLICATION = 'shopping_delight.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
