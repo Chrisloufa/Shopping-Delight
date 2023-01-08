33ac6ae3cda68ba6d42e218d083d68812997ec6c
Chrisloufa <chris.mcgarry3@gmail.com>
2023-01-08 14:56:26 +0000

    created toast messages for products

--- bag/views.py
+++ bag/views.py
@@ -15,6 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -23,6 +24,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag.')
 
     request.session['bag'] = bag
     print(request.session['bag'])
