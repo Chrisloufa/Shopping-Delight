dca697e24b3b7590dc9c2e29d3aa8900a21fa0c1
Chrisloufa <chris.mcgarry3@gmail.com>
2023-01-09 18:07:07 +0000

    added basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,8 +5,8 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
-var clientSecret = $("#id_client_secret").text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -24,7 +24,9 @@ var style = {
     iconColor: "#dc3545",
   },
 };
-var card = elements.create("card", { style: style });
+var card = elements.create("card", {
+  style: style
+});
 card.mount("#card-element");
 
 // Handle realtime validation errors on the card element
@@ -41,4 +43,48 @@ card.addEventListener("change", function (event) {
   } else {
     errorDiv.textContent = "";
   }
+});
+
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+  ev.preventDefault();
+  // If the client secret was rendered server-side as a data-secret attribute
+  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+  card.update({
+    'disabled': true
+  });
+  $('#submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then(function (result) {
+    if (result.error) {
+      var errorDiv = document.getElementById("card-errors");
+      // Show error to your customer (for example, insufficient funds)
+      var html = `
+            <p class="icon" role="alert">
+            <i class="fa-solid fa-circle-xmark"></i>
+            </p>
+            <p>${result.error.message}</p>
+        `;
+      $(errorDiv).html(html);
+      card.update({
+        'disabled': false
+      });
+      $('#submit-button').attr('disabled', false);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        form.onsubmit();
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+      }
+    }
+  });
 });
