8ee208b4e746cd3dacd896199d8216d31a4c1999
Chrisloufa <chris.mcgarry3@gmail.com>
2023-01-10 15:16:42 +0000

    added checkout success logic

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -78,7 +78,7 @@ form.addEventListener('submit', function (ev) {
     } else {
       // The payment has been processed!
       if (result.paymentIntent.status === 'succeeded') {
-        form.onsubmit();
+        form.submit();
         // Show a success message to your customer
         // There's a risk of the customer closing the window before callback
         // execution. Set up a webhook or plugin to listen for the
