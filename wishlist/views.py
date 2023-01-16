40ed11da09e1385f95dff4ec9b9e8d6bf4108ead
Chrisloufa <chris.mcgarry3@gmail.com>
2023-01-16 15:44:41 +0000

    tidy up html for wishlist and now working wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,45 +1,45 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Wishlist
 from products.models import Product
 from profiles.models import UserProfile
+from .models import Wishlist
 
 
 @login_required
 def get_wishlist(request):
-    """ A view to return the wishlist page """
+    """
+    A view to render the users wishlist
+    """
+    wishlist = None
+    try:
+        wishlist = Wishlist.objects.get(user=request.user.userprofile)
+    except Wishlist.DoesNotExist:
+        pass
 
-    template = 'wishlist/wishlist.html'
+    context = {
+        'wishlist': wishlist,
+    }
 
-    return render(request, template)
-
+    print(wishlist)
+    return render(request, 'wishlist/wishlist.html', context=context)
+    
 
 @login_required
 def add_to_wishlist(request, product_id):
-    """ A view to add a product to user wishlist """
-
-    # get current product
+    """
+    Add a product from the store to the
+    wishlist for the logged in user
+    """
     product = get_object_or_404(Product, pk=product_id)
 
-    # get current user
     user = get_object_or_404(UserProfile, user=request.user)
 
-    # get user wishlist otherwise create wishlist
     wishlist, created = Wishlist.objects.get_or_create(user=user)
-
-    # check if product exists in user wishlist
-    check_duplicate = bool(
-        Wishlist.objects.filter(user=user, products=product))
-
-    # if product exists in user wishlist, inform user, else, add
-    #  to user wishlist
-    if check_duplicate:
-        messages.info(request, f'{product.name} already in your wishlist.')
-    else:
-        wishlist.products.add(product)
-        messages.success(request, f'{product.name} added to your wishlist.')
+    print(created)
+    # Add product to the wishlist
+    wishlist.products.add(product)
 
     return redirect(reverse('product_detail', args=[product.id]))
 
