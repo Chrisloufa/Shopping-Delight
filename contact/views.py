23e80a4d62e33dd798f71f0b5fbce2d71735e349
Chrisloufa <chris.mcgarry3@gmail.com>
2023-02-17 10:02:00 +0000

    added new hos pass

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,10 @@
+""" Contact app views """
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.contrib import messages
 
 # Create your views here.
+
 
 def contact(request):
     """View to return contact us page"""
